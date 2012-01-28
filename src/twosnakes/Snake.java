95044b3ee8b88309564de380068132781399a3b7
Koh Ushijima <koh.ush89@gmail.com>
2012-01-28 00:50:40 -0800

    Snake.java updated. P1Snake.java added

--- src/twosnakes/Snake.java
+++ src/twosnakes/Snake.java
@@ -8,8 +8,11 @@ public interface Snake {
 	 * @param left_or_right left == false, right == false
 	 */
 	public void move(int distance, boolean left_or_right);
-	public void get_long();
+	public void get_longer();
 	public void get_shorter();
 	public void get_fat();
 	public void get_skinny();
+	
+	public void set_speed();
+	public double get_speed();
 }
