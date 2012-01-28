59b19771d5fe3121f4f2b29fa0af24ba19b8e9d4
Koh Ushijima <koh.ush89@gmail.com>
2012-01-28 00:52:42 -0800

    oops

---
+++ src/twosnakes/P1Snake.java
@@ -0,0 +1,66 @@
+package twosnakes;
+
+public class P1Snake implements Snake{
+
+	class Head{
+		
+	}
+	class Body{
+		
+	}
+	class Tail{
+		
+	}
+	
+	private int[][] pivit;
+	private double speed = 0.0;
+	
+	@Override
+	/**
+	 * 
+	 * @param distance
+	 * @param left_or_right
+	 */
+	public void move(int distance, boolean left_or_right) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get_longer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get_shorter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get_fat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void get_skinny() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void set_speed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double get_speed() {
+		// TODO Auto-generated method stub
+		return this.speed;
+	}
+	
+
+}
