e231a3944f48ca7916c113ef881da5c2e234e6a0
Jovan Ristic <jristic42@gmail.com>
2012-01-28 12:11:04 -0800

    changes to snake and interface, added some game update stuff

--- src/twosnakes/P1Snake.java
+++ src/twosnakes/P1Snake.java
@@ -1,48 +1,35 @@
 package twosnakes;
 
-import java.awt.event.KeyEvent;
-
-public class P1Snake implements Snake{
-	
-	
-
-	class Head{
-		private double headPicWidth = -10;
-		private double headPicLeng = -10;
-		private double[] rHeadPiv = new double [2];
-		public double[] lHeadPiv = new double [2];
+public class P1Snake implements Snake 
+{
+	class Head
+	{
+		private double headPicWidth = 0;
+		private double headPicLeng = 0;
+		private Vector rHeadPiv = new Vector();
+		public Vector lHeadPiv = new Vector();
 	}
-	class Body{
-		private double bodyPicWidth= -10;
-	    private double bodyPicLeng= -10;
-		private double[] bodyPiv = new double [2]; 
+	
+	class Body
+	{
+		private double bodyPicWidth= 0;
+	    private double bodyPicLeng= 0;
+		private Vector bodyPiv = new Vector(); 
 	}
-	class Tail{
+	
+	class Tail
+	{
 		// Init width and length with the picture size
-		private double[] tailPiv = new double [2];
-		private final double tailPicWidth = -10;
-		private final double tailPicLeng = -10;
-		
-		public Tail(){
-			tailPiv[0] = 0; //
-			tailPiv[1] = 0.5*tailPicLeng;
-		}
-		
-		
+		private Vector lTailPiv = new Vector();
+		private Vector rTailPiv = new Vector();
+		private final double tailPicWidth = 0;
+		private final double tailPicLeng = 0;
 	}
 	
-	private double[] pivit;
-	private double speed = 0.0;
-	private final double move_per_click;
-	private int num_clicks;
-	private double[] vector; //this keeps track of the current angle that pivit is moving to.
+	private double speed = 0;
 	
-	public P1Snake(){
-		move_per_click = 1.0;
-		
-		vector = new double[1];
-		vector[0] = 0.0;
-		vector[1] = 1.0;
+	public P1Snake(Vector head, Vector facing)
+	{
 		
 	}
 	@Override
@@ -51,7 +38,8 @@ public class P1Snake implements Snake{
 	 * @param distance
 	 * @param left_or_right
 	 */
-	public void move(int distance, boolean left_or_right) {
+	public void move(double distance, Vector direction) 
+	{
 		// TODO Auto-generated method stub
 		
 	}
@@ -93,12 +81,9 @@ public class P1Snake implements Snake{
 		return this.speed;
 	}
 	
-	public int get_click(KeyEvent ka){
-		return this.num_clicks;
-	}
 	@Override
 	public void set_speed() {
-		
+		this.speed = speed;
 	}
 
 }
--- src/twosnakes/Snake.java
+++ src/twosnakes/Snake.java
@@ -7,12 +7,9 @@ public interface Snake {
 	 * @param distance 
 	 * @param left_or_right left == false, right == false
 	 */
-	public void move(int distance, boolean left_or_right);
+	public void move(double distance, Vector direction);
 	public void get_longer();
 	public void get_shorter();
 	public void get_fat();
 	public void get_skinny();
-	
-	public void set_speed();
-	public double get_speed();
 }
