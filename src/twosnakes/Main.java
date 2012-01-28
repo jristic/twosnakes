52a03b5aa48f20c732ba5ca48392b8053a87785e
Jovan Ristic <jristic42@gmail.com>
2012-01-27 22:47:22 -0800

    added functioning game files, need to be refined.

--- src/twosnakes/Main.java
+++ src/twosnakes/Main.java
@@ -1,21 +1,78 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package twosnakes;
 
-/**
- *
- * @author jovan
- */
-public class Main
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main extends JFrame implements WindowListener
 {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args)
+	private static int DEFAULT_FPS = 80;
+	public static MainPanel bp; // where the game is drawn
+
+	public Main(int period)
 	{
-		// TODO code application logic here
+		super("Ninjas vs. Pirates");
+		makeGUI(period);
+		addWindowListener(this);
+		pack();
+		setResizable(false);
+		setVisible(true);
+	}
+
+	private void makeGUI(int period)
+	{
+		Container c = getContentPane();    // default BorderLayout used
+		bp = new MainPanel(this, period);
+		c.add(bp, "Center");
+	}
+
+	// ----------------- window listener methods -------------
+	public void windowActivated(WindowEvent e)
+	{
+		bp.resumeGame();
+	}
+
+	public void windowDeactivated(WindowEvent e)
+	{
+		bp.pauseGame();
+	}
+
+	public void windowDeiconified(WindowEvent e)
+	{
+		bp.resumeGame();
+	}
+
+	public void windowIconified(WindowEvent e)
+	{
+		bp.pauseGame();
+	}
+
+	public void windowClosing(WindowEvent e)
+	{
+		bp.stopGame();
+	}
+
+	public void windowClosed(WindowEvent e)
+	{
+	}
+
+	public void windowOpened(WindowEvent e)
+	{
+	}
+
+	// ----------------------------------------------------
+	public static void main(String args[])
+	{
+		int fps = DEFAULT_FPS;
+		if (args.length != 0)
+		{
+			fps = Integer.parseInt(args[0]);
+		}
+
+		int period = (int) 1000.0 / fps;
+		System.out.println("fps: " + fps + "; period: " + period + " ms");
+
+		new Main(period);    // ms
 	}
 }
