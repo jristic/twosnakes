3dfb0cc4af94b440d37a2f06bc86556475c9a606
Koh Ushijima <koh.ush89@gmail.com>
2012-01-28 14:34:08 -0800

    Collision.java added

---
+++ src/twosnakes/Collision.java
@@ -0,0 +1,23 @@
+package twosnakes;
+
+public class Collision implements Event{
+
+	@Override
+	public void playSound(Sound s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void animation(Animation a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSnake(Snake s) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
