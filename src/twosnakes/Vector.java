069e4d9f1d102dad9c04ff163e64295d8cf223c9
Koh Ushijima <koh.ush89@gmail.com>
2012-01-28 12:24:56 -0800

    merging

---
+++ src/twosnakes/Vector.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twosnakes;
+
+/**
+ *
+ * @author jovan
+ */
+public class Vector
+{
+	public Vector()
+	{
+		this.x = this.y = 0;
+	}
+	public Vector(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public double x, y;
+}
