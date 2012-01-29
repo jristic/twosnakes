8f480db6f83b1fac0aea381b51946adafb233855
Koh Ushijima <koh.ush89@gmail.com>
2012-01-28 16:51:10 -0800

    Update setDirection is updated.

--- src/twosnakes/Update.java
+++ src/twosnakes/Update.java
@@ -19,7 +19,49 @@ public class Update
 	
 	void processKeyPress(KeyEvent e)
 	{
-		
+		//get the new direction vector based on which key (left or right) is pressed.
+		Vector currentDirection = state.snake1.getDirection();
+		double x = currentDirection.x;
+		double y = currentDirection.y;
+		Vector delta;
+		if(e.equals(KeyEvent.VK_LEFT)){
+			if(x >= 0.0 && y >= 0.0){
+				x = x - y/10.0;
+				y = y + x/10.0;
+			}
+			else if(x >= 0.0 && y < 0.0){
+				x = x - y/10.0;
+				y = y + x/10.0;
+			}
+			else if(x < 0.0 && y >= 0.0){
+				x = x - y/10.0; 
+				y = y + x/10.0;
+			}
+			else if(x < 0.0 && y < 0.0){
+				x = x - y/10.0;
+				y = y + x/10.0;
+			}
+		}
+		else if (e.equals(KeyEvent.VK_RIGHT)){
+			
+			if(x >= 0.0 && y >= 0.0){
+				x = x + y/10.0;
+				y = y - x/10.0;
+			}
+			else if(x >= 0.0 && y < 0.0){
+				x = x + y/10.0;
+				y = y - x/10.0;
+			}
+			else if(x < 0.0 && y >= 0.0){
+				x = x - y/10.0; 
+				y = y - x/10.0;
+			}
+			else if(x < 0.0 && y < 0.0){
+				x = x + y/10.0;
+				y = y + x/10.0;
+			}
+		}
+		state.snake1.setDirection(new Vector(x,y));
 	}
 
 	void processKeyRelease(KeyEvent e)
